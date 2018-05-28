2a0c8b55f826da4cd17af5cadf92cdc389c1ca4f
Syraxyo <38350315+Syraxyo@users.noreply.github.com>
2018-05-28 21:17:44 +0200

    Add files via upload
    Halforder komplett

--- src/HalfOrder.java
+++ src/HalfOrder.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Set;
 
 public class HalfOrder
 
@@ -8,54 +9,43 @@ public class HalfOrder
 	public void init()
 	{
 
-		ArrayList<Node> nodelist = new ArrayList<Node>();
-		// Erstellung Knoten
-		nodelist.add(new Node("Knoten 1 "));
-		nodelist.add(new Node("Knoten 2 "));
-		nodelist.add(new Node("Knoten 3 "));
-		nodelist.add(new Node("Knoten 4 "));
-		nodelist.add(new Node("Knoten 5 "));
-		nodelist.add(new Node("Knoten 6 "));
-		// X und Y Werte an Knoten vergeben " Noch " selbst
-		//	k1.name("Knoten1");
-		nodelist.get(0).setX(100);
-		nodelist.get(0).setY(200);
 
-		nodelist.get(1).setX(150);
-		nodelist.get(1).setY(300);
+		ArrayList<Node> nodelist = createNodelistExampleOne();
+	//	printNodes(nodelist);
 
-		nodelist.get(2).setX(150);
-		nodelist.get(2).setY(100);
+		System.out.println();
 
-		nodelist.get(3).setX(200);
-		nodelist.get(3).setY(300);
-
-		nodelist.get(4).setX(200);
-		nodelist.get(4).setY(100);
-
-		nodelist.get(5).setX(250);
-		nodelist.get(5).setY(200);
+		ArrayList<Edge> edgelist = createEdgelistExampleOne(nodelist);
+	//	printEdges(edgelist);
 
 
-		// Ausgabe  Knoten in Console
-		for(int i = 0; i < nodelist.size();i++)
+		ArrayList<ArrayList <Node>> listOfLists = computeHalfOrder(getFirstNode(nodelist), nodelist, edgelist);
+
+	}
+
+
+
+	public Node getFirstNode (ArrayList <Node> nodelist)
+	{		
+		return nodelist.get(0);
+
+	}
+
+	private void printEdges(ArrayList<Edge> edgelist, String descr) 
+	{
+		System.out.println("Edges: " + descr);
+		
+		for(int i = 0; i < edgelist.size(); i++)
 		{
-			if (nodelist.size() >= i)
-			{
-				System.out.println(nodelist.get(i));
-			}
-			else{
-				System.out.println("--------");
-			}
-
+				System.out.println(edgelist.get(i));
 		}
+		
+		System.out.println("--------");
+	}
 
-		System.out.println("");
-
+	private ArrayList<Edge> createEdgelistExampleOne(ArrayList <Node> nodelist) 
+	{
 		ArrayList<Edge> edgelist = new ArrayList<Edge>();
-
-
-		// Erstellung Kanten
 		edgelist.add(new Edge (8.0));
 		edgelist.add(new Edge (10.0));
 		edgelist.add(new Edge (5.0));
@@ -64,17 +54,6 @@ public class HalfOrder
 		edgelist.add(new Edge (9.0));
 		edgelist.add(new Edge (20.5));
 
-
-		edgelist.get(0);
-		edgelist.get(1);
-		edgelist.get(2);
-		edgelist.get(3);
-		edgelist.get(4);
-		edgelist.get(5);
-		edgelist.get(6);
-
-
-		// Erstellung Kanten Quelle / Ziel
 		edgelist.get(0).setDestination(nodelist.get(1));
 		edgelist.get(0).setSource(nodelist.get(0));
 
@@ -96,39 +75,31 @@ public class HalfOrder
 		edgelist.get(6).setDestination(nodelist.get(5));
 		edgelist.get(6).setSource(nodelist.get(0));
 
-		// Ausgabe von Knoten x zu Knoten y
-		for(int i = 0; i < edgelist.size();i++)
+		return edgelist;
+	}
+
+	private void printNodes(ArrayList<Node> nodelist, String descr) 
+	{
+		System.out.println("Nodes: " + descr);
+
+		for(int i = 0; i < nodelist.size(); i++)
 		{
-			if (edgelist.size() >= i)
-			{
-				System.out.println(edgelist.get(i));
-			}
-			else
-			{
-				System.out.println("--------");
-			}
-
+			System.out.println(nodelist.get(i));
 		}
+		
+		System.out.println("--------");
+	}
 
-		System.out.println();
-		System.out.println("---Start---");																// Ausgabe Anfangsknoten
-		System.out.println(nodelist.get(0));
-
-		// Ausgabe weiterer NextNodeListen in Console // von Schleife unten
-		/*	
-		ArrayList<Node> nextNode = ComputeNextNodes(nodelist.get(0), edgelist);					
-		printNodeList(nextNode);
-
-		ArrayList<Node> nextNode1 = ComputeNextNodes(nextNode.get(0), edgelist);
-		printNodeList(nextNode1);
-
-		ArrayList<Node> nextNode2 = ComputeNextNodes(nextNode.get(1), edgelist);
-		printNodeList(nextNode2);
-		System.out.println("");
-
-		System.out.println("---End---");																// Ausgabe Endknoten
-		System.out.println(nodelist.get(5));
-		 */
+	private ArrayList<Node> createNodelistExampleOne() {
+		ArrayList<Node> nodelist = new ArrayList<Node>();
+		// Erstellung Knoten
+		nodelist.add(new Node("Knoten 1 "));
+		nodelist.add(new Node("Knoten 2 "));
+		nodelist.add(new Node("Knoten 3 "));
+		nodelist.add(new Node("Knoten 4 "));
+		nodelist.add(new Node("Knoten 5 "));
+		nodelist.add(new Node("Knoten 6 "));
+		return nodelist;
 	}
 
 	public Boolean checkIfDoubleEdges (ArrayList<Edge> edgelist, Node destination)
@@ -148,14 +119,14 @@ public class HalfOrder
 				}
 
 			}
-			
+
 		}
 		return false;
 	}
 
-	public ArrayList<Node> ComputeNextNodes (Node source, ArrayList<Edge> edgelist)						// Berechnung von nächsten Knoten
+	public ArrayList<Node> computeNextNodes (Node source, ArrayList<Edge> edgelist)						// Berechnung von nächsten Knoten
 	{
-		ArrayList<Node> nextNode = new ArrayList<Node>();
+		ArrayList<Node> nextNodes = new ArrayList<Node>();
 
 		for(int i = 0; i < edgelist.size(); i++)
 		{
@@ -163,11 +134,12 @@ public class HalfOrder
 
 			if(e.getSource() == source)
 			{
-				nextNode.add(e.getDestination());
+				nextNodes.add(e.getDestination());
 			}
+
 		}
-		
-		return nextNode;
+
+		return nextNodes;
 	}
 
 	public ArrayList<Node> addNodeIfNotRedundant (Node nodeToAdd, ArrayList<Node> nodelist)
@@ -179,33 +151,69 @@ public class HalfOrder
 				return nodelist;
 			}
 		}
-		
+
 		nodelist.add(nodeToAdd);
 		return nodelist;
 	}
 
-	public ArrayList<ArrayList <Node>> printList (ArrayList<Node> nodelist)
+	public ArrayList<ArrayList<Node>> computeHalfOrder(Node firstNode, ArrayList <Node> nodelist, ArrayList <Edge> edgelist)
 	{
-		ArrayList<ArrayList<Node>> print = new ArrayList <ArrayList <Node>>();
+		ArrayList<ArrayList <Node>> listOfLists = new ArrayList<ArrayList <Node>>();
+		ArrayList<Node> allNodesProcessed = new ArrayList<Node>();
+
+		ArrayList<Node> stepTarget = computeNextNodes(firstNode, edgelist);
+		listOfLists.add(stepTarget);
+		allNodesProcessed.addAll(stepTarget);
 		
+		int i = 1;
 		
-		for (int i = 0; i < nodelist.size(); i++)
+		printNodes(stepTarget,"" + i);
+		printNodes(allNodesProcessed, i + " all");
+
+		while (stepTarget.size() > 0)
 		{
-			System.out.println(nodelist.get(i));
+			ArrayList<Node> stepSource = stepTarget;
+			stepTarget = new ArrayList<Node>();
+
+			for(int j = 0; j < stepSource.size(); j++)
+			{
+				ArrayList <Node> partStep = computeNextNodes(stepSource.get(j), edgelist);
+				magicalAdd(partStep, stepTarget, allNodesProcessed);
+			}
+
+			if (stepTarget.size() > 0)
+			{
+				listOfLists.add(stepTarget);
+				
+				printNodes(stepTarget, "" + ++i);
+				printNodes(allNodesProcessed, i + " all");
+			}
+			
+			
 		}
-		System.out.println(nodelist);
-		return print;
-		
+
+		return listOfLists;
 	}
 
-	public void printNodeList(ArrayList<Node> nextNode)											// Schleife zur Ausgabe von Knotenlistne
+
+	public void magicalAdd(ArrayList<Node> source, ArrayList<Node> destination, ArrayList<Node> alreadyProcessed)
 	{
-		System.out.println();
-		System.out.println("---NextNodeList---");
-		for(int i = 0; i < nextNode.size(); i++)
+		for (Node node : source) 
 		{
-			System.out.println(nextNode.get(i));	
+			if (!alreadyProcessed.contains(node))
+			{
+				destination.add(node);
+				alreadyProcessed.add(node);
+			}
 		}
+	}
+
+
+	public void printHalfOrder (ArrayList <ArrayList <Node>> computeHalforder)
+	{
+
 
 	}
+
+
 }
