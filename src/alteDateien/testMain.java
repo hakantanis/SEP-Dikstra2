7e52f6f710c36e4bec0a87dc9463784797c7116a
hakantanis <38211753+hakantanis@users.noreply.github.com>
2018-09-21 09:41:51 +0200

    Add files via upload

---
+++ src/alteDateien/testMain.java
@@ -0,0 +1,45 @@
+/*import javax.swing.*;
+import java.awt.*;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+
+public class testMain extends JApplet
+{
+    Edge edge = new Edge(10);
+    Edge edge1 = new Edge(10);
+    Edge edge2 = new Edge(10);
+    Node node = new Node("Kevin", 100,200) ;
+    Node node1 = new Node("Hakan", 200, 150);
+    Node node2 = new Node("Tim", 200, 250);
+    Node node3 = new Node("Ben", 300, 150);
+
+
+    public void init(final Stage primaryStage)
+    {
+        edge.setDestination(node);
+        edge.setSource(node1);
+
+        edge1.setDestination(node1);
+        edge1.setSource(node2);
+
+        edge2.setDestination(node);
+        edge2.setSource(node3);
+
+
+    }
+
+    public void paint(Graphics g)
+    {
+        edge.paint(g);
+        edge1.paint(g);
+        edge2.paint(g);
+        node.paint(g);
+        node1.paint(g);
+        node2.paint(g);
+        node3.paint(g);
+    }
+
+}
+*/
